edf18b92c4cdf1736e71c8a9683f84a8476f79ea
Paris Tao <paristao1989@gmail.com>
2015-10-22 13:59:13 +0800

    引入jquery和extjs

---
+++ stocks-business/src/test/java/com/chinesedreamer/stocks/business/analyze/service/WarningServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.chinesedreamer.stocks.business.analyze.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WarningServiceImplTest {
+
+	@Test
+	public void testKdjWarning() {
+		fail("Not yet implemented");
+	}
+
+}
